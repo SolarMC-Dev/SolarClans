82e45a335dbc182ad0bb09d0d1ac877a1ad6ef94
Zone-Infinity <sohamdokhale1602@gmail.com>
2021-07-15 22:12:53 +0530

    Basic Plugin

---
+++ src/main/java/gg/solarmc/clans/SolarClans.java
@@ -0,0 +1,15 @@
+package gg.solarmc.clans;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SolarClans extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
