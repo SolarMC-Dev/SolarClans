3588222381a59830d4bb6e00409905bbedaf221c
Zone-Infinity <sohamdokhale1602@gmail.com>
2021-08-09 07:03:08 +0530

    Cancel the packet event and remove ClanType..

--- src/main/java/gg/solarmc/clans/packets/NameTagPacket.java
+++ src/main/java/gg/solarmc/clans/packets/NameTagPacket.java
@@ -16,12 +16,6 @@ import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.InvocationTargetException;
 
-import static gg.solarmc.clans.packets.ClanType.MEMBER;
-
-enum ClanType {
-    NEUTRAL, MEMBER, ALLY, ENEMY
-}
-
 public class NameTagPacket {
     private final PacketType TYPE = PacketType.Play.Server.TAGS;
     private final Logger LOGGER = LoggerFactory.getLogger(NameTagPacket.class);
@@ -39,11 +33,11 @@ public class NameTagPacket {
                     @Override
                     public void onPacketSending(PacketEvent event) {
                         if (event.getPacketType() != TYPE) return;
+                        event.setCancelled(true);
 
                         PacketContainer packet = event.getPacket();
                         Entity entity = packet.getEntityModifier(event).read(0);
                         if (!(entity instanceof Player other)) return;
-                        ClanType clanType = ClanType.NEUTRAL;
 
                         Player player = event.getPlayer();
 
@@ -51,17 +45,14 @@ public class NameTagPacket {
                         Clan otherClan = other.getSolarPlayer().getData(ClansKey.INSTANCE).currentClan().orElse(null);
 
                         if (playerClan != null && otherClan != null) {
-                            if (playerClan.equals(otherClan)) clanType = MEMBER;
-                            else if (otherClan.equals(playerClan.currentAllyClan().orElse(null)))
-                                clanType = ClanType.ALLY;
+                            if (playerClan.equals(otherClan)) {
+                                other.displayName(other.displayName().color(NamedTextColor.GREEN));
+                            }
+                            else if (otherClan.equals(playerClan.currentAllyClan().orElse(null))) {
+                                other.displayName(other.displayName().color(NamedTextColor.LIGHT_PURPLE));
+                            }
                             // TODO:
-                            // else if(otherClan.equals(playerClan.currentEnemyClan().orElse(null))) clanType = ClanType.ENEMY;
-                        }
-
-                        switch (clanType){
-                            case MEMBER -> other.displayName(other.displayName().color(NamedTextColor.GREEN));
-                            case ALLY -> other.displayName(other.displayName().color(NamedTextColor.LIGHT_PURPLE));
-                            case ENEMY ->  other.displayName(other.displayName().color(NamedTextColor.RED));
+                            // else if(otherClan.equals(playerClan.currentEnemyClan().orElse(null))) {other.displayName(other.displayName().color(NamedTextColor.RED));}
                         }
 
                         packet.getEntityModifier(event).write(0, other);
